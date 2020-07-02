1430bdf491eedc0abbf801233b7eb04c77710fe7
Erick Walker <selfmade60@Ericks-MacBook-Pro.local>
2020-07-01 21:40:20 -0400

    update 02

--- main.js
+++ main.js
@@ -3,59 +3,82 @@ let coin = {
     state: 0,
     flip: function() {
 
-         this.state = Math.random()
+        this.state = Math.random()
 
          return this.state
     },
 
 
-     toString: function() {
+    toString: function() {
         
-         if(this.state >= 0.5) {
+     if(this.state <= 0.5) {
+
+           this.state = Math.floor(this.state)
+
             return 'Heads'
-        } else {
+      } else {
+
+            this.state = Math.ceil(this.state)
+
            return 'Tails'
         }
     },
       toHTML: function() {
-        let image = document.createElement('img');
-        
-        if(this.state >= 0.5) {
-            image.src = "images/coinheads.jpg"
-        } else {
-            image.src = "images/Cointails.jpg"
-        }
 
+        let image = document.createElement('img');
+
+        image.width = "100"
+        image.height = "100"
+        
+        if(this.state <= 0.5) {
+            image.src = "./images/coinheads.jpg"
+        } else {
+            image.src = "./images/Cointails.jpg"
+
+        }
         return image;
     } 
-};
+}; 
 
+console.log(coin.state)
 console.log(coin.flip())
 console.log(coin.toString())
 
  function display20Flips() {
 
+    let list = document.createElement('ul')
+
     for(let i = 0; i < 20; i ++) {
+    
+       let item = document.createElement('li')
         
        let randomFlip = coin.toString()
 
-       document.body.textContent = randomFlip
+       item.textContent = randomFlip
+
+       document.body.append(item)
     }
 }
 
 display20Flips()
 
 
-function display20Images() {
+ function display20Images() {
     for( let index = 0; index < 20; index ++) {
        
+       // let item = document.createElement('li')
+
         let randomImage = coin.toHTML()
 
-        document.body.textContent = randomImage
+        //item.textContent = randomImage
+
+        document.body.append(randomImage)
     }
 } 
 
 display20Images()
+
+
 /* GOALS OF PROJECT:
 
 The General Idea: 
